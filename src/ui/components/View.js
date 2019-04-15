9b1aa1b677cfe71fdbfbf8ff73cd583e24c2ed80
you@example.com <you@example.com>
2019-04-15 20:04:57 +0200

    inserted edit mode

--- src/ui/components/View.js
+++ src/ui/components/View.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 
 import '../styles/App.css';
-import { AppLoader } from "./Send";
 
 export class SavedItemsTable extends Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
 
         this.state = {
             items: [],
             baseUrl: '',
-            update: true
+            update: true,
+            apps: []
         }
         
         let uri = window.location.protocol;
@@ -21,21 +20,9 @@ export class SavedItemsTable extends Component {
         this.setState({baseUrl: uri});
 
         this.updateList = this.updateList.bind(this);
-        this.handleSend = this.handleSend.bind(this);
     }
 
-    componentWillReceiveProps(newProps) {
-        console.log('New Props: ', newProps.app)
-        this.setState({
-            measures:[],
-            dimensions: [],
-            app: newProps.app
-        }, (r) => {
-            console.log('My new State: ', this.state)
-            this.updateList();
-        })
-        
-        
+    componentWillReceiveProps(newProps) {   
     }
 
     shouldComponentUpdate() {
@@ -43,27 +30,23 @@ export class SavedItemsTable extends Component {
     }
 
     componentWillMount(){
-        this.updateList();
-    }
+        let uri = this.state.baseUrl + '/api/qsgetdoclist';
 
-    handleSend(id, method, object) {
-        let sendUrl = this.state.baseUrl + '/api/editobject'
-
-        let body = {
-            id: id,
-            method: method,
-            object: object
-        }
-
-        console.log(body)
-
-        fetch(sendUrl, {
-            method: 'post',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        }).then(() => {
+        fetch(uri)
+        .then((list) => {
+            return list.json();
+        })
+        .then((apps) => {
+            return apps.map((app, idx) => {
+                return (
+                    <option key={idx} value={app.qDocId}>{app.qDocName}</option>
+                )
+            })
+        })
+        .then((options) => {
+            this.setState({
+                apps: options
+            })
             this.updateList();
         })
     }
@@ -79,36 +62,19 @@ export class SavedItemsTable extends Component {
             console.log(response)
             let savedItems = response.map((item, idx) => {
                 return (
-                    <tr>
-                        <td><b>{item.name}</b></td>
-                        <td>{item.description}</td>
-                        <td>{item.type}</td>
-                        <td>{item.app}</td>
-                        <td>{item.objectid}</td>
-                        <td><code>{item.definition}</code></td>
-                        <td>
-                            <input
-                                class="btn btn-danger"
-                                type="button"
-                                value="Delete"
-                                onClick={() => {this.handleSend(item.id, 'delete')}}
-                            />
-                        </td>
-                        <td>
-                            <input
-                                class="btn btn-warning"
-                                type="button"
-                                value="Duplicate"
-                                onClick={() => {this.handleSend(item.id, 'duplicate')}}
-                            />
-                        </td>
-                        <td>
-                            <AppSelector 
-                                object={item.object}
-                                id={item.id}
-                            />
-                        </td>
-                    </tr>
+                        <ItemLine 
+                            key=        {item.id}
+                            name=       {item.name}
+                            description={item.description}
+                            type=       {item.type}
+                            app=        {item.app}
+                            objectid=   {item.objectid}
+                            definition= {item.definition}
+                            object=     {item.object}
+                            line_id=    {item.id}
+                            update=     {this.updateList}
+                            apps=       {this.state.apps}
+                        />
                 )
             })
             this.setState({items: savedItems});
@@ -152,7 +118,7 @@ export class AppSelector extends Component {
 
         this.state = {
             app: '',
-            apps: [],
+            apps: this.props.apps,
             value: 'App',
             id: this.props.id,
             object: this.props.object, 
@@ -160,6 +126,7 @@ export class AppSelector extends Component {
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSend = this.handleSend.bind(this);
+        this.setBtnClass = this.setBtnClass.bind(this);
 
         let uri = window.location.protocol;
         uri += '//' + window.location.hostname;
@@ -170,24 +137,6 @@ export class AppSelector extends Component {
     }
 
     componentWillMount() {        
-        let uri = this.state.baseUrl + '/api/qsgetdoclist';
-
-        fetch(uri)
-        .then((list) => {
-            return list.json();
-        })
-        .then((apps) => {
-            return apps.map((app, idx) => {
-                return (
-                    <option key={idx} value={app.qDocId}>{app.qDocName}</option>
-                )
-            })
-        })
-        .then((options) => {
-            this.setState({
-                apps: options
-            })
-        })
     }
 
     shouldComponentUpdate() {
@@ -210,25 +159,23 @@ export class AppSelector extends Component {
             object: object,
             app: this.state.app
         }
-
-        console.log(body)
-
         fetch(sendUrl, {
             method: 'post',
             headers: {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify(body)
-        }).then(() => {
-            // this.updateList();
         })
     }
 
+    setBtnClass(targetClass){
+        return (this.props.edit) ? targetClass + ' disabled':targetClass;
+    }
+
     render() {
-        console.log(this.state);
         return(
             <div class="input-group">         
-                <select class="form-control"
+                <select class={this.setBtnClass("form-control")}
                     value={this.state.value} 
                     onChange={this.handleChange}
                     >
@@ -237,13 +184,152 @@ export class AppSelector extends Component {
                 </select>
                 <span class="input-group-btn">
                     <input
-                        class="btn btn-info"
+                        class={this.setBtnClass("btn btn-info")}
                         type="button"
                         value="Export"
-                        onClick={() => {this.handleSend(this.state.id, 'export', this.state.object)}}
+                        onClick={(this.props.edit) ? '': () => {this.handleSend(this.state.id, 'export', this.state.object)}}
                     />
                 </span>
             </div>
+        )
+    }
+}
+
+export class ItemLine extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            id: this.props.line_id,
+            name: this.props.name,
+            type: this.props.type,
+            app: this.props.app,
+            objectid: this.props.objectid,
+            definition: this.props.definition,
+            object: this.props.object,
+
+            edit: false,
+
+            baseUrl: ''
+        }
+        this.handleSend = this.handleSend.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.setTdOrInput = this.setTdOrInput.bind(this);
+        this.setBtnClass = this.setBtnClass.bind(this);
+        this.toggleBtnClass = this.toggleBtnClass.bind(this);
+
+        let uri = window.location.protocol;
+        uri += '//' + window.location.hostname;
+        uri += (window.location.port.length > 0) ? (':' + window.location.port):'';
+
+        this.setState({baseUrl: uri});
+    }
+    
+    handleSend(id, method, object) {
+        let sendUrl = this.state.baseUrl + '/api/editobject'
+
+        let body = {
+            id: id,
+            method: method,
+            object: object
+        }
+
+        fetch(sendUrl, {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then(() => {
+            console.log('Update')
+            this.props.update();
+        })
+    }
+
+    handleEdit() {
+        console.log('Editing')
+        this.setState({edit: !this.state.edit})
+    }
+
+    setTdOrInput(type, text){
+        if (this.state.edit) {
+            var jsx='';
+            switch(type) {
+                case 'textarea':
+                jsx = <textarea>{text}</textarea>
+                break;
+
+                case 'input':
+                jsx = <input value={text} />
+                break;
+            }
+            return jsx;
+        } else {
+            return (
+                text
+            )
+        }
+    }
+
+    setBtnClass(targetClass){
+        return (this.state.edit) ? targetClass + ' disabled':targetClass;
+    }
+
+    toggleBtnClass() {
+        return (this.state.edit) ? "btn btn-success":"btn btn-info"
+    }
+
+    render() {
+        return (
+            <tr>
+                <td><b>{this.setTdOrInput('input', this.state.name)}</b></td>
+                <td>{this.setTdOrInput('textarea',this.state.description)}</td>
+                <td>{this.state.type}</td>
+                <td>{this.state.app}</td>
+                <td>{this.state.objectid}</td>
+                <td><code>{this.setTdOrInput('textarea', this.state.definition)}</code></td>
+                <td>
+                    <button
+                        class={this.toggleBtnClass()}
+                        type="button"
+                        value="X"
+                        title="Edit Item"
+                        onClick={this.handleEdit}
+                    >
+                        <i class="fas fa-pencil-alt" />
+                    </button>
+                </td>
+                <td>
+                    <button
+                        class={this.setBtnClass("btn btn-danger")}
+                        type="button"
+                        value="X"
+                        title="Delete Item"
+                        onClick={(this.state.edit) ? '':() =>{this.handleSend(this.state.id, 'delete')}}
+                    >
+                        <i class="fas fa-trash-alt" />
+                    </button>
+                </td>
+                <td>
+                    <button
+                        class={this.setBtnClass("btn btn-warning")}
+                        type="button"
+                        value="x2"
+                        title="Duplicate Item"
+                        onClick={(this.state.edit) ? '':() => {this.handleSend(this.state.id, 'duplicate')}}
+                    >
+                        <i class="fas fa-angle-double-right" />
+                    </button>
+                </td>
+                <td>
+                    <AppSelector 
+                        object={this.state.object}
+                        id={this.state.id}
+                        apps={this.props.apps}
+                        edit={this.state.edit}
+                    />
+                </td>
+            </tr>
         )
     }
 }
