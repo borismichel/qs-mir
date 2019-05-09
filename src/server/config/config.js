44c08420b1ad6ef08f3bc288650d1eaf23c57949
Boris Michel <borismichel@Boriss-MacBook-Pro.local>
2019-05-09 18:54:45 +0200

    replaced cfg

---
+++ src/server/config/config.js
@@ -0,0 +1,10 @@
+// Webserver Config 
+
+export var port = 1212;
+
+// Qlik Sense Stuff
+
+export var qlikCertificateDir = './src/server/cert/';
+export var qlikUser     = 'UserDirectory=INTERNAL;UserId=SA_API';
+export var qlikServer   = null;
+export var qlikIsSrv    = false;
